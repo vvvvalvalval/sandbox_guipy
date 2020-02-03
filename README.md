2569c976fb21a061dda1304beb674063f059e402
Valentin Waeselynck <vwaeselynck@ap2s.fr>
2020-02-03 18:31:38 +0100

    First Hello World from WxPython tutorial

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+## Install dependencies
+
+```
+conda install -c anaconda python.app
+conda install -c conda-forge wxpy
+```
+
+
+## Run the app
+
+``` 
+pythonw app.py
+```
+
---
+++ app.py
@@ -0,0 +1,16 @@
+## From WxPython tutorial: https://www.wxpython.org/pages/overview/#hello-world
+
+# First things, first. Import the wxPython package.
+import wx
+
+# Next, create an application object.
+app = wx.App()
+
+# Then a frame.
+frm = wx.Frame(None, title="Hello World")
+
+# Show it.
+frm.Show()
+
+# Start the event loop.
+app.MainLoop()
